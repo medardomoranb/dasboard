7d6bbf6367457a8bebfb2b6912acafa905ee43e2
medardomoranb <119766136+medardomoranb@users.noreply.github.com>
2024-11-13 13:42:11 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+
+import './App.css'
+
+import Grid from '@mui/material/Grid2' 
+
+
+function App() {
+
+  return (
+    <>
+          <Grid container spacing={5}>
+
+{/* Indicadores */}
+<Grid>Elemento: Indicador 1</Grid>
+<Grid>Elemento: Indicador 2</Grid>
+<Grid>Elemento: Indicador 3</Grid>
+<Grid>Elemento: Indicador 4</Grid>
+
+{/* Tabla */}
+<Grid>Elemento: Tabla</Grid>
+
+{/* Gráfico */}
+<Grid>Elemento: Gráfico 1</Grid>
+
+</Grid>
+    </>
+  )
+}
+
+export default App
