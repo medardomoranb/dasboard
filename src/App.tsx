79603073b8604ce8b612634710c7e685c0b964a1
medardomoranb <mrmoran@espol.edu.ec>
2024-11-25 13:41:22 -0500

    guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-
+import IndicatorWeather from './components/IndicatorWeather';
 
 import './App.css'
 
 import Grid from '@mui/material/Grid2' 
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
+import LineChartWeather from './components/LineChartWeather';
+
 
 
 function App() {
@@ -10,20 +14,45 @@ function App() {
   return (
     <>
           <Grid container spacing={5}>
-
-{/* Indicadores */}
-<Grid>Elemento: Indicador 1</Grid>
-<Grid>Elemento: Indicador 2</Grid>
-<Grid>Elemento: Indicador 3</Grid>
-<Grid>Elemento: Indicador 4</Grid>
-
-{/* Tabla */}
-<Grid>Elemento: Tabla</Grid>
-
-{/* Gráfico */}
-<Grid>Elemento: Gráfico 1</Grid>
-
-</Grid>
+                
+                {/* Indicadores  */}
+                <Grid size={{xs:12, xl:3}}>
+                    <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
+                </Grid>
+   
+                <Grid size={{xs:12, xl:3}}>
+                    <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+                </Grid>
+                   
+                <Grid size={{xs:12, xl:3}}>
+                    <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+                </Grid>
+                   
+                <Grid size={{xs:12, xl:3}}>
+                    <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+                </Grid>
+                   
+                    
+             {/* Tabla */}
+             <Grid size={{xs:12, xl:8}}>
+                    
+                    {/* Grid Anidado */}
+                    <Grid container spacing={2}>
+                        <Grid size={{ xs: 12, xl: 3 }}>
+                            <ControlWeather/>
+                        </Grid>
+                        <Grid size={{ xs: 12, xl: 9 }}>
+                            <TableWeather/>
+                        </Grid>
+                    </Grid>
+   
+                </Grid>
+               {/* Gráfico */}
+             <Grid size={{xs:12, xl:4}}>
+                 <LineChartWeather/>
+             </Grid>  
+      
+            </Grid>
     </>
   )
 }
